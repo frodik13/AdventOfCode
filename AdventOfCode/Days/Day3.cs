b415fb75125f90df07cef2b8291bd9830f8d6d12
Fedor <frodo-66@yandex.ru>
2024-12-05 16:19:51 +0400

    complete day3

--- AdventOfCode/Days/Day3.cs
+++ AdventOfCode/Days/Day3.cs
@@ -5,6 +5,7 @@ namespace AdventOfCode.Days;
 public partial class Day3() : BaseDay("Day3.txt")
 {
     private readonly Regex _regex = InstructionRegex();
+    private readonly Regex _instructionPartTwoRegex = InstructionPartTwoRegex();
 
     public override Task<long> ExecutePartOne()
     {
@@ -19,24 +20,51 @@ public partial class Day3() : BaseDay("Day3.txt")
     
     public override Task<long> ExecutePartTwo()
     {
-        throw new NotImplementedException();
+        var result = 0L;
+        var skipValues = false;
+        foreach (var line in Input)
+        {
+            var matches = _instructionPartTwoRegex.Matches(line);
+            foreach (var match in matches.Cast<Match>())
+            {
+                switch (match.Value)
+                {
+                    case "don't()":
+                        skipValues = true;
+                        continue;
+                    case "do()":
+                        skipValues = false;
+                        continue;
+                }
+                
+                if (skipValues) continue;
+                
+                result += ParseInstruction(match.Value);
+            }
+        }
+        
+        return Task.FromResult(result);
     }
-    
+
     private static long CalculateInstructions(MatchCollection matches)
     {
         return matches
             .AsParallel()
-            .Select(match =>
-            {
-                var instruction = match.Value;
-                instruction = instruction.Replace("mul(", "").Replace(")", "");
-                var digits = instruction.Split(',');
-                var multiply = long.Parse(digits[0]) * long.Parse(digits[1]);
-                return multiply;
-            })
+            .Select(match => ParseInstruction(match.Value))
             .Sum();
     }
+
+    private static long ParseInstruction(string instruction)
+    {
+        instruction = instruction.Replace("mul(", "").Replace(")", "");
+        var digits = instruction.Split(',');
+        var multiply = long.Parse(digits[0]) * long.Parse(digits[1]);
+        return multiply;
+    }
     
-    [GeneratedRegex(@"mul\(\d*,\d*\)")]
+    [GeneratedRegex(@"mul\(\d+,\d+\)")]
     private static partial Regex InstructionRegex();
+    
+    [GeneratedRegex(@"mul\(\d+,\d+\)|don't\(\)|do\(\)")]
+    private static partial Regex InstructionPartTwoRegex();
 }
