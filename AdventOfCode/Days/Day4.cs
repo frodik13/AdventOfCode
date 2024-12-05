7ca4f41b962464718a4587edee4dbff1189eb884
Fedor <frodo-66@yandex.ru>
2024-12-05 17:15:56 +0400

    complete part one

---
+++ AdventOfCode/Days/Day4.cs
@@ -0,0 +1,71 @@
+namespace AdventOfCode.Days;
+
+public class Day4() : BaseDay("Day4.txt")
+{
+    public override Task<long> ExecutePartTwo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<long> ExecutePartOne()
+    {
+        var result = 0L;
+        
+        for (var i = 0; i < Input.Length; i++)
+            for (var j = 0; j < Input[0].Length; j++)
+                if (Input[i][j] == 'X')
+                    result += FindCountWords(i, j);
+
+        return Task.FromResult(result);
+    }
+
+    private long FindCountWords(int i, int j)
+    {
+        return Enum.GetValues<Direction>().LongCount(direction => IsFindWord(i, j, direction));
+    }
+
+    private bool IsFindWord(int i, int j, Direction direction)
+    {
+        var nextPoint = NextPoint(direction, i, j);
+        if (!IsCorrectChar(nextPoint.i, nextPoint.j, 'M')) return false;
+        
+        nextPoint = NextPoint(direction, nextPoint.i, nextPoint.j);
+        if (!IsCorrectChar(nextPoint.i, nextPoint.j, 'A')) return false;
+        
+        nextPoint = NextPoint(direction, nextPoint.i, nextPoint.j);
+        return IsCorrectChar(nextPoint.i, nextPoint.j, 'S');
+    }
+    
+    private bool IsCorrectChar(int i, int j, char c)
+    {
+        return i < Input.Length && j < Input[0].Length && i >= 0 && j >= 0 && Input[i][j] == c;
+    }
+    
+    private static (int i, int j) NextPoint(Direction direction, int i, int j)
+    {
+        return direction switch
+        {
+            Direction.Down => (i + 1, j),
+            Direction.Up => (i - 1, j),
+            Direction.Left => (i, j + 1),
+            Direction.Right => (i, j - 1),
+            Direction.DownRight => (i + 1, j + 1),
+            Direction.DownLeft => (i + 1, j - 1),
+            Direction.UpLeft => (i - 1, j - 1),
+            Direction.UpRight => (i - 1, j + 1),
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+    
+    private enum Direction
+    {
+        Down,
+        Up,
+        Left,
+        Right,
+        DownRight,
+        DownLeft,
+        UpLeft,
+        UpRight,
+    }
+}
